a331a1b2aaa033fed092b70144d386f9d3286cf7
Jared Castillo <jarecast@espol.edu.ec>
2024-08-08 10:37:40 -0500

    prediction-section implemented

--- src/components/Navbars/NavBar.js
+++ src/components/Navbars/NavBar.js
@@ -1,13 +1,13 @@
 import logo from "assets/images/logo.png"
 
-function NavBar() {
+function NavBar(){
 
-    return (<nav id="navbar">
+    return(<nav id="navbar">
         <div className="nav-container">
             <div className="logo-container"><img id="logo" src={logo}></img>
             </div>
             <div className="nav-buttons">
-                <ul id="nav-buttons-list">
+                <ul id = "nav-buttons-list">
                     <il className="nav-button"><a class="nav-link" href="#prediction-section">Predict</a></il>
                     <il className="nav-button"><a class="nav-link" href="#prediction-section">Statistics</a></il>
                     <il className="nav-button"><a class="nav-link" href="#prediction-section">Graphics</a></il>
