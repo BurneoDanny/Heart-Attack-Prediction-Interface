b77ec95a200d65439ab77cf7cf6b4b2de92cf993
“Danny <developer@solinal.org>
2024-08-12 20:55:32 -0500

    Models almost finished

--- src/pages/Wayared/Models/Models.js
+++ src/pages/Wayared/Models/Models.js
@@ -5,85 +5,24 @@ import Protonin_high from "assets/images/troponin_high.jpg";
 import Protonin_normal from "assets/images/troponin_normal.jpg";
 import Impulse_high from "assets/images/impulse_before.jpg";
 import Impulse_normal from "assets/images/impulse_after.jpg";
+import { importation } from "./Info";
+import { importation2 } from "./Info";
+import { filtering } from "./Info";
+import { penalty } from "./Info";
+import { solver } from "./Info";
+import { c_value } from "./Info";
+import { best_variables } from "./Info";
+import { regression_training_loop } from "./Info";
+import { kernels } from "./Info";
+import { gamma } from "./Info";
+import { C_values_svm } from "./Info";
+import { best_variables_svm } from "./Info";
+import { svm_training_loop } from "./Info";
+import { DecisionTree } from "./Info";
+import { RandomForest } from "./Info";
+import { Link } from "react-router-dom";
 
 export default function Models() {
-  const importation = `
-        # Librerias comunes 
-        from sklearn.metrics import accuracy_score
-        from sklearn.model_selection import train_test_split
-        import pandas as pd
-        import pickle
-        # Para regresion logistica
-        from sklearn.linear_model import LogisticRegression
-        #Para SVM
-        from sklearn.svm import SVC
-    `;
-
-  const filtering = `
-# borrar ruido de pulsos exageradamente altos.
-datos_filtrados = datos[datos['impulse'] <= 300] 
-# borrar ruido de troponina exageradamente alta en casos negativos
-datos_filtrados = datos_filtrados[~((datos_filtrados['troponin'] > 9) & (datos_filtrados['class'] == 'negativa'))] 
-  `;
-
-  const penalty = `penalties = ['l1', 'l2', 'elasticnet', 'none']
-l1_ratios = [0.1, 0.5, 0.7]  # Solo para elasticnet   
-best_l1_ratio = None  # Para elasticnet`;
-  const solver = ` solvers = ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']`;
-  const c_value = ` C_values = [0.01, 0.1, 1, 10, 100]`;
-  const best_variables = `
-  best_penalty = str()
-  best_solver = str()
-  best_C = float()
-  best_acc = 0
-  `;
-
-  const regression_training_loop = `
-  # Bucle para probar todas las combinaciones de hiperparámetros
-  for penalty in penalties:
-      for solver in solvers:
-          for C in C_values:
-              # Algunos solvers no soportan ciertas penalizaciones
-              if penalty == 'l1' and solver not in ['liblinear', 'saga']:
-                  continue
-              if penalty == 'elasticnet' and solver != 'saga':
-                  continue
-              if penalty == 'none' and solver in ['liblinear', 'saga']:
-                  continue
-  
-              # Si es elasticnet, se prueba con diferentes l1_ratios
-              if penalty == 'elasticnet':
-                  for l1_ratio in l1_ratios:
-                      LR = LogisticRegression(penalty=penalty, C=C, solver=solver, l1_ratio=l1_ratio, random_state=0, max_iter=1000)
-                      LR.fit(X_train, y_train)
-                      y_pred = LR.predict(X_test)
-                      score = accuracy_score(y_test, y_pred)
-  
-                      if score > best_acc:
-                          best_acc = score
-                          best_penalty = penalty
-                          best_solver = solver
-                          best_C = C
-                          best_l1_ratio = l1_ratio
-              else:
-                LR = LogisticRegression(penalty=penalty, C=C, solver=solver, random_state=0, max_iter=10000)
-                LR.fit(X_train, y_train)
-                y_pred = LR.predict(X_test)
-  
-                score = accuracy_score(y_test, y_pred)
-  
-                if score > best_acc:
-                    best_acc = score
-                    best_penalty = penalty
-                    best_solver = solver
-                    best_C = C
-  
-  # Resultados
-  print('Best Penalty : ', best_penalty)
-  print('Best Solver : ', best_solver)
-  print('Best C : ', best_C)
-  print('Accuracy Score : ', best_acc)`;
-
   return (
     <div className="overflow-y-auto h-screen text-black-pearl-50 text-justify">
       <div className="font-extrabold my-6 text-2xl text-center">
@@ -98,16 +37,27 @@ best_l1_ratio = None  # Para elasticnet`;
           powerful tools for classification tasks. In this project, we applied
           these models to predict the risk of acute myocardial infarction using
           a clinical dataset. We will present the code that demonstrates how
-          these models were implemented, covering data preprocessing, model
-          training, evaluation, and key results. This walkthrough will provide a
-          clear understanding of the AI techniques employed in this analysis.
+          these models (not all of them) were implemented, covering data
+          preprocessing, model training, and evaluation. This walkthrough will
+          provide a clear understanding of the AI techniques employed in this
+          analysis.{" "}
+          <Link
+            to={"/wayared?menu=result"}
+            className="text-black-pearl-500 cursor-pointer"
+          >
+            Results here.
+          </Link>
         </p>
         <div className="flex justify-center items-center">
           <p className="flex-1 mx-24">
             We chose to work on Google Colab, as this tool allows for easy
             implementation of the models and helps achieve the project’s
             objectives. The{" "}
-            <a className="text-black-pearl-500 cursor-pointer">
+            <a
+              className="text-black-pearl-500 cursor-pointer"
+              href="https://colab.research.google.com/drive/18G379SW0Qmi03kETqb1cw7TKg6EHT8Yn?usp=sharing"
+              target="_blank"
+            >
               <i>Google Colab notebook</i>
             </a>{" "}
             is available in view-only mode, allowing for detailed inspection of
@@ -119,27 +69,6 @@ best_l1_ratio = None  # Para elasticnet`;
           <div className="flex-1">
             <img src={ColabImage} className="rounded-sm" />
           </div>
-        </div>
-      </div>
-      <div className="p-6 m-6  flex flex-col justify-center items-center rounded-sm text-left bg-black-pearl-950 text-base border-2 border-black-pearl-500">
-        <p className="mb-4 text-center font-bold">The liraries used include:</p>
-        <div className="flex gap-2">
-          <CodeBlock
-            text={importation}
-            language="python"
-            showLineNumbers={true}
-            theme={dracula}
-            wrapLines={true}
-            codeBlock
-          />
-          <CodeBlock
-            text={importation}
-            language="python"
-            showLineNumbers={true}
-            theme={dracula}
-            wrapLines={true}
-            codeBlock
-          />
         </div>
       </div>
       <div className="p-6 m-6 rounded-sm text-base border-2 bg-black-pearl-950 border-black-pearl-500 flex flex-col justify-center items-center gap-2">
@@ -190,69 +119,19 @@ best_l1_ratio = None  # Para elasticnet`;
           />
         </div>
       </div>
-      <div className="p-6 m-6 text-base border-2 flex gap-4 flex-col rounded-sm border-black-pearl-500 bg-black-pearl-950">
-        <p>
-          Let's begin by explaining the Logistic Regression model implemented in
-          Python using scikit-learn, along with a detailed overview of its
-          hyperparameters.
-        </p>
-        <div className="shadow-lg rounded-lg p-6 text-left flex flex-col gap-2">
-          <p>
-            The hyperparameters of the Logistic Regression model include
-            penalty, solver, and C value.
-          </p>
-          <li>
-            The penalty parameter specifies the norm used in the penalization.
-          </li>
+      <div className="p-6 m-6  flex flex-col justify-center items-center rounded-sm text-left bg-black-pearl-950 text-base border-2 border-black-pearl-500">
+        <p className="mb-4 text-center font-bold">The liraries used include:</p>
+        <div className="flex gap-2">
           <CodeBlock
-            text={penalty}
+            text={importation}
             language="python"
             showLineNumbers={true}
             theme={dracula}
             wrapLines={true}
             codeBlock
           />
-          <li>
-            The solver parameter specifies the algorithm used for optimization.
-          </li>
           <CodeBlock
-            text={solver}
-            language="python"
-            showLineNumbers={true}
-            theme={dracula}
-            wrapLines={true}
-            codeBlock
-          />
-          <li>
-            The C value is the inverse of regularization strength. Smaller
-            values specify stronger regularization.
-          </li>
-          <CodeBlock
-            text={c_value}
-            language="python"
-            showLineNumbers={true}
-            theme={dracula}
-            wrapLines={true}
-            codeBlock
-          />
-          <p>
-            The following variables are used to store the best hyperparameters
-            and accuracy score.
-          </p>
-          <CodeBlock
-            text={best_variables}
-            language="python"
-            showLineNumbers={true}
-            theme={dracula}
-            wrapLines={true}
-            codeBlock
-          />
-          <p>
-            The code snippet below demonstrates the training loop for the
-            Logistic Regression model.
-          </p>
-          <CodeBlock
-            text={regression_training_loop}
+            text={importation2}
             language="python"
             showLineNumbers={true}
             theme={dracula}
@@ -334,20 +213,51 @@ best_l1_ratio = None  # Para elasticnet`;
       </div>
       <div className="p-6 m-6 text-base border-2 flex gap-4 flex-col rounded-sm border-black-pearl-500 bg-black-pearl-950">
         <p>
-          Now we go throuh the SVM (Selector Vercto Model) model implemented in
+          This time, we will go through the Decision Tree model implemented in
+          Python witch is slightly different from the previous implementation.
+        </p>
+        <div className="shadow-lg rounded-lg p-6 text-left flex flex-col gap-2">
+          <p>
+            In this implementation, we utilize a Decision Tree model that allows
+            for more complex decision boundaries compared to linear models. The
+            code includes data preprocessing steps such as label encoding and
+            employs K-Fold cross-validation to assess model performance. This
+            method ensures that the model is robust and generalizes well to
+            unseen data by training and testing on different subsets of the
+            dataset.
+          </p>
+
+          <p>
+            The code snippet below demonstrates the training loop for the
+            Decision Tree Model.
+          </p>
+          <CodeBlock
+            text={DecisionTree}
+            language="python"
+            showLineNumbers={true}
+            theme={dracula}
+            wrapLines={true}
+            codeBlock
+          />
+        </div>
+      </div>
+      <div className="p-6 m-6 text-base border-2 flex gap-4 flex-col rounded-sm border-black-pearl-500 bg-black-pearl-950">
+        <p>
+          Now we go throuh the SVM (Support vector machine) model implemented in
           Python using scikit-learn, along with a detailed overview of its
           hyperparameters.
         </p>
         <div className="shadow-lg rounded-lg p-6 text-left flex flex-col gap-2">
           <p>
-            The hyperparameters of the SVM model include penalty, solver, and C
-            value.
+            The hyperparameters of the SVM model include kernel, C value, and
+            gamma value.
           </p>
           <li>
-            The penalty parameter specifies the norm used in the penalization.
+            The kernel parameter specifies the kernel type used in the
+            algorithm.
           </li>
           <CodeBlock
-            text={penalty}
+            text={kernels}
             language="python"
             showLineNumbers={true}
             theme={dracula}
@@ -355,10 +265,11 @@ best_l1_ratio = None  # Para elasticnet`;
             codeBlock
           />
           <li>
-            The solver parameter specifies the algorithm used for optimization.
+            The gamma parameter specifies the kernel coefficient for 'rbf',
+            'poly', and 'sigmoid'.
           </li>
           <CodeBlock
-            text={solver}
+            text={gamma}
             language="python"
             showLineNumbers={true}
             theme={dracula}
@@ -370,7 +281,7 @@ best_l1_ratio = None  # Para elasticnet`;
             values specify stronger regularization.
           </li>
           <CodeBlock
-            text={c_value}
+            text={C_values_svm}
             language="python"
             showLineNumbers={true}
             theme={dracula}
@@ -382,7 +293,7 @@ best_l1_ratio = None  # Para elasticnet`;
             and accuracy score.
           </p>
           <CodeBlock
-            text={best_variables}
+            text={best_variables_svm}
             language="python"
             showLineNumbers={true}
             theme={dracula}
@@ -390,11 +301,11 @@ best_l1_ratio = None  # Para elasticnet`;
             codeBlock
           />
           <p>
-            The code snippet below demonstrates the training loop for the
-            Logistic Regression model.
+            The code snippet below demonstrates the training loop for the SVM
+            model.
           </p>
           <CodeBlock
-            text={regression_training_loop}
+            text={svm_training_loop}
             language="python"
             showLineNumbers={true}
             theme={dracula}
@@ -405,10 +316,85 @@ best_l1_ratio = None  # Para elasticnet`;
       </div>
       <div className="p-6 m-6 text-base border-2 flex gap-4 flex-col rounded-sm border-black-pearl-500 bg-black-pearl-950">
         <p>
-          Let's begin by explaining the Logistic Regression model implemented in
-          Python using scikit-learn, along with a detailed overview of its
-          hyperparameters.
+          In this section, we will explore the Random Forest model implemented
+          in Python, which enhances the capabilities of a single Decision Tree
+          by combining multiple trees to improve accuracy and robustness.
         </p>
+        <div className="shadow-lg rounded-lg p-6 text-left flex flex-col gap-2">
+          <p>
+            This implementation utilizes a Random Forest model that aggregates
+            predictions from multiple decision trees, reducing the risk of
+            overfitting and providing more stable predictions. The code includes
+            data preprocessing steps such as label encoding and employs K-Fold
+            cross-validation to evaluate the model's performance across
+            different subsets of the dataset. Additionally, the model can
+            provide probability estimates for class predictions, allowing for a
+            deeper analysis of the results.
+          </p>
+
+          <p>
+            The code snippet below demonstrates the training loop for the Random
+            Forest model.
+          </p>
+          <CodeBlock
+            text={RandomForest}
+            language="python"
+            showLineNumbers={true}
+            theme={dracula}
+            wrapLines={true}
+            codeBlock
+          />
+        </div>
+      </div>
+
+      <div className="p-6 m-6 text-base border-2 flex gap-4 flex-col rounded-sm border-black-pearl-500 bg-black-pearl-950">
+        <p>
+          In this section, we have covered several machine learning models,
+          including Decision Trees and Random Forests, implemented in Python.
+          Each of these models offers unique advantages and can be tailored for
+          various classification tasks.
+        </p>
+        <div className="shadow-lg rounded-lg p-6 text-left flex flex-col gap-2">
+          <p>
+            For those interested in exploring additional models and their
+            implementations, including MLP, GBM, and more, you can find all the
+            details in the Google Colab notebook linked below. The notebook
+            includes thorough explanations, code snippets, and visualizations
+            that will help you understand the performance and applicability of
+            each model.
+          </p>
+
+          <p>
+            We encourage you to experiment with the code and datasets provided,
+            as hands-on practice is a great way to deepen your understanding of
+            machine learning techniques.
+          </p>
+
+          <a
+            href="https://colab.research.google.com/drive/18G379SW0Qmi03kETqb1cw7TKg6EHT8Yn?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 underline"
+          >
+            Models Google Colab Notebook
+          </a>
+          <a
+            href="https://colab.research.google.com/drive/1nshA1A0gbU2o0GLeL-pyBehk-ywM0aN4?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 underline"
+          >
+            Graphs Google Colab Notebook
+          </a>
+          <a
+            href="https://www.kaggle.com/code/nimapourmoradi/heartattack-classification"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 underline"
+          >
+            Dataset
+          </a>
+        </div>
       </div>
     </div>
   );
